e081823cfec136aab30379c69cf621fb15ac17f0
Supun Ranwaka <supun.15@cse.mrt.ac.lk>
2018-07-06 16:10:13 +0530

    test

--- navigation/navigate.tsx
+++ navigation/navigate.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StackNavigator, } from 'react-navigation';
 import { Button } from 'react-native-elements';
-import Icon from 'react-native-vector-icons/FontAwesome';
+
 
 import login from '../screens/login';
 import contacts from '../screens/contacts';
